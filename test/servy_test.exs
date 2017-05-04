8f24e5ee954051bee707404f18629c803c80477a
Mario Uher <uher.mario@gmail.com>
2017-05-04 08:47:21 +0200

    Refactor tests

--- test/servy_test.exs
+++ test/servy_test.exs
@@ -1,28 +1,24 @@
 defmodule ServyTest do
   use ExUnit.Case, async: true
 
-  test "/wildthings" do
-    response = Servy.Handler.handle("""
-    GET /wildthings HTTP/1.1
+  defp get(path) do
+    Servy.Handler.handle("""
+    GET #{path} HTTP/1.1
     Host: example.com
     User-Agent: ExampleBrowser/1.0
     Accept: */*
 
     """)
+  end
 
+  test "/wildthings" do
+    response = get("/wildthings")
     assert response =~ "Bears, Lions, Tigers"
     assert response =~ "Content-Length: 20"
   end
 
   test "/international-wildthings" do
-    response = Servy.Handler.handle("""
-    GET /international-wildthings HTTP/1.1
-    Host: example.com
-    User-Agent: ExampleBrowser/1.0
-    Accept: */*
-
-    """)
-
+    response = get("/international-wildthings")
     assert response =~ "Beärs, Liöns, Tigers"
     assert response =~ "Content-Length: 22"
   end
