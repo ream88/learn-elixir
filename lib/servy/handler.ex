6ea7aeafca2fe98eb3d8efde5c62b063bf48d4cc
Mario Uher <uher.mario@gmail.com>
2017-05-04 08:00:27 +0200

    Bears, Lions and Tigers

---
+++ lib/servy/handler.ex
@@ -0,0 +1,35 @@
+defmodule Servy.Handler do
+  def handle(request) do
+    conv = parse(request)
+    conv = route(conv)
+    format_response(conv)
+  end
+
+  def parse(request) do
+    %{ method: "GET", path: "/wildthings", resp_body: "" }
+  end
+
+  def route(conv) do
+    %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions and Tigers" }
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+IO.puts Servy.Handler.handle(request)
--- test/servy_test.exs
+++ test/servy_test.exs
@@ -1,8 +1,3 @@
 defmodule ServyTest do
   use ExUnit.Case
-  doctest Servy
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
