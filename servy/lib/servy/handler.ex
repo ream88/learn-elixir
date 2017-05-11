88da612703aefa4d9811de2df20474539ab2ef64
Mario Uher <uher.mario@gmail.com>
2017-05-11 08:48:33 +0200

    Refactor

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -13,7 +13,7 @@ defmodule Servy.Handler do
       |> Enum.at(0)
       |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method, path: path, body: "" }
   end
 
   defp route(conv) do
@@ -21,24 +21,24 @@ defmodule Servy.Handler do
   end
 
   defp route(conv, "/wildthings") do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{ conv | body: "Bears, Lions, Tigers" }
   end
 
   defp route(conv, "/international-wildthings") do
-    %{ conv | resp_body: "Beärs, Liöns, Tigers" }
+    %{ conv | body: "Beärs, Liöns, Tigers" }
   end
 
   defp route(conv, "/bears") do
-    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+    %{ conv | body: "Teddy, Smokey, Paddington" }
   end
 
   defp format_response(conv) do
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: #{byte_size(conv.resp_body)}
+    Content-Length: #{byte_size(conv.body)}
 
-    #{conv.resp_body}
+    #{conv.body}
     """
   end
 end
