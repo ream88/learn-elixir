2ee9e95b7426624df9a6e8b480f9f9abd6c39add
Mario Uher <uher.mario@gmail.com>
2017-05-27 10:20:54 +0200

    Allow viewing of messages

--- messages/lib/messages.ex
+++ messages/lib/messages.ex
@@ -27,6 +27,22 @@ defmodule Messages do
     GenServer.cast(pid, {:message, message})
   end
 
+  @doc """
+  Views all messages.
+
+      iex> {:ok, pid} = Messages.start
+      iex> Messages.view(pid)
+      []
+
+      iex> {:ok, pid} = Messages.start
+      iex> Messages.send(pid, "Hey, how are you?")
+      iex> Messages.view(pid)
+      ["Hey, how are you?"]
+  """
+  def view(pid) do
+    GenServer.call(pid, :view)
+  end
+
   # Server
   def init(state) do
     {:ok, state}
@@ -40,5 +56,18 @@ defmodule Messages do
   """
   def handle_cast({:message, message}, state) do
     {:noreply, [message | state]}
+  end
+
+  @doc """
+  Handles view calls.
+
+      iex> Messages.handle_call(:view, nil, [])
+      {:reply, [], []}
+
+      iex> Messages.handle_call(:view, nil, ["Hey, how are you?"])
+      {:reply, ["Hey, how are you?"], ["Hey, how are you?"]}
+  """
+  def handle_call(:view, _from, state) do
+    {:reply, state, state}
   end
 end
