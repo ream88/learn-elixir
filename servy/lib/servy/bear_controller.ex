2c329a3522b95108d0dad990cea0f6c91f5d54ab
Mario Uher <uher.mario@gmail.com>
2017-06-12 09:38:47 +0200

    Refactor code

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -1,26 +1,29 @@
 defmodule Servy.BearController do
+  alias Servy.Bear
   alias Servy.Conv
   alias Servy.Wildthings
 
+  defp bear_name(bear) do
+    bear.name
+  end
+
   def index(%Conv{} = conv, _) do
-    name = fn(bear) -> bear.name end
     body =
       Wildthings.list_bears
-      |> Enum.filter(fn(bear) -> bear.type == "Grizzly" end)
-      |> Enum.sort(fn(bear1, bear2) -> bear1.name <= bear2.name end)
-      |> Enum.map(&name.(&1))
+      |> Enum.filter(&Bear.is_grizzly/1)
+      |> Enum.sort(&Bear.order_by_name_asc/2)
+      |> Enum.map(&bear_name/1)
       |> Enum.join(", ")
 
     %{ conv | body: body }
   end
 
   def show(%Conv{} = conv, %{ "id" => id }) do
-    name = fn(bear) -> bear.name end
     bear = Wildthings.get_bear(id)
 
     cond do
       bear == nil -> %{ conv | body: "No bear with id #{id} found", status: 404 }
-      bear != nil -> %{ conv | body: name.(bear) }
+      bear != nil -> %{ conv | body: bear_name(bear) }
     end
   end
 end
