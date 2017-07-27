5ff5c5de10914c77c5a5c7e3658baec19250049c
Mario Uher <uher.mario@gmail.com>
2017-07-27 09:52:00 +0200

    Improve performance by using EEx.function_from_file

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -1,35 +1,25 @@
 defmodule Servy.BearController do
   alias Servy.Bear
+  alias Servy.BearView
   alias Servy.Conv
   alias Servy.Wildthings
-
-  @templates_path Path.expand("templates", File.cwd!)
 
   def index(%Conv{} = conv, _) do
     bears =
       Wildthings.list_bears
       |> Enum.sort(&Bear.order_by_name_asc/2)
 
-    render(conv, "index.eex", bears: bears)
+    %{conv | body: BearView.index(bears)}
   end
 
   def show(%Conv{} = conv, %{"id" => id}) do
     case Wildthings.get_bear(id) do
       nil -> %{conv | body: "No bear with id #{id} found", status: 404}
-      bear -> render(conv, "show.eex", bear: bear)
+      bear -> %{conv | body: BearView.show(bear)}
     end
   end
 
   def delete(%Conv{} = conv, _) do
     %{conv | body: "Deleting a bear is forbidden!", status: 403}
-  end
-
-  defp render(%Conv{} = conv, template, bindings \\ []) do
-    body =
-      @templates_path
-      |> Path.join(template)
-      |> EEx.eval_file(bindings)
-
-    %{conv | body: body}
   end
 end
