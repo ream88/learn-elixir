234e7ca2cbe533cc003af038572aa8cc39b7e40a
Mario Uher <uher.mario@gmail.com>
2017-08-29 10:56:26 +0200

    Implement MyList.each/2

--- learnelixir/lib/my_list.ex
+++ learnelixir/lib/my_list.ex
@@ -10,4 +10,13 @@ defmodule MyList do
   defp length([_ | list], count) do
     length(list, count + 1)
   end
+
+  def each([], _fun) do
+    :ok
+  end
+
+  def each([item | list], fun) do
+    fun.(item)
+    each(list, fun)
+  end
 end
--- learnelixir/test/my_list_test.exs
+++ learnelixir/test/my_list_test.exs
@@ -11,4 +11,14 @@ defmodule MyListTest do
       assert MyList.length([1, 2, 3]) == 3
     end
   end
+
+  describe "each" do
+    test "is given an empty list" do
+      assert MyList.each([], &IO.puts/1) == :ok
+    end
+
+    test "is given a list with 3 items" do
+      assert MyList.each([1, 2, 3], &IO.puts/1) == :ok
+    end
+  end
 end
