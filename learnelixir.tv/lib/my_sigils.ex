a7bc5ca4712f2606ff5c3c45562423eb8796d592
Mario Uher <uher.mario@gmail.com>
2017-08-30 08:30:05 +0200

    Override built-in sigil

--- learnelixir.tv/lib/my_sigils.ex
+++ learnelixir.tv/lib/my_sigils.ex
@@ -1,7 +1,14 @@
 defmodule MySigils do
+  import Kernel, except: [sigil_w: 2]
+
   def sigil_u(content, _) do
     content
     |> String.split
     |> Enum.map(&String.upcase/1)
+  end
+
+  def sigil_w(content, _) do
+    content
+    |> String.split
   end
 end
--- learnelixir.tv/test/my_sigils_test.exs
+++ learnelixir.tv/test/my_sigils_test.exs
@@ -1,8 +1,13 @@
 defmodule MySigilsTest do
   use ExUnit.Case
+  import Kernel, except: [sigil_w: 2]
   import MySigils
 
   test "custom u sigil" do
     assert ~u[foo bar] == ["FOO", "BAR"]
+  end
+
+  test "overriden w sigil does ignore modifiers" do
+    assert ~w[foo bar]a == ["foo", "bar"]
   end
 end
